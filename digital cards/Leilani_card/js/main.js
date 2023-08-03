b4fb26a66bd9fdf9e0bbffbde6ff152d30c591f1
Reddsgn <karen.crz.rojas@gmail.com>
2023-08-03 15:40:54 -0600

    new folder
    Digital interactive cards project

---
+++ digital cards/Leilani_card/js/main.js
@@ -0,0 +1,30 @@
+//CODE CASCADE
+//BOTON SONIDO
+function play(id) {
+  const audioElement = document.getElementById(id);
+
+resetAll();
+
+  if (audioElement.paused) {
+      audioElement.play();
+  }
+}
+
+function resetAll() {
+const audioElements = document.querySelectorAll('audio[id^="sound-"]');
+
+audioElements.forEach(audioElement => {
+  audioElement.pause();
+  audioElement.currentTime = 0;
+});
+}
+
+$(function(){
+  $(document).bind("contextmenu",function(e){
+      return false;
+  });
+});
+
+AOS.init({
+  duration: 1200,
+})
