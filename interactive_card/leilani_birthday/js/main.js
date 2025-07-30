add885d69649edd17d5712af3f917c9356d7fc81
KarencitasDescalzas <karen.crz.rojas@gmail.com>
2025-07-30 15:55:28 -0600

    v1Interactive card
    Interactive card

---
+++ interactive_card/leilani_birthday/js/main.js
@@ -0,0 +1,31 @@
+
+//BOTON SONIDO
+function play(id) {
+  const audioElement = document.getElementById(id);
+
+resetAll();
+
+  if (audioElement.paused) {
+      audioElement.play();
+  }
+}
+
+function resetAll() {
+const audioElements = document.querySelectorAll('audio[id^="sound-"]');
+
+audioElements.forEach(audioElement => {
+  audioElement.pause();
+  audioElement.currentTime = 0;
+});
+}
+
+
+$(function(){
+$(document).bind("contextmenu",function(e){
+    return false;
+ });
+});
+
+AOS.init({
+  duration: 1200,
+})
